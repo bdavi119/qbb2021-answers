2d54f6df91396547a77ce0eb344aef2e9d929c87
Brendon Davis <bdavi119@jhu.edu>
2021-09-02 09:43:05 -0400

    Refined gtf parser to exclude duplicates and debugged advanced exercise part 2

--- day3-lunch/n_nearest_genes.py
+++ day3-lunch/n_nearest_genes.py
@@ -3,11 +3,8 @@
 Brendon Davis
 CMDB Bootcamp Day 3 Lunch - Advanced Exercises Part 2
 
-This program requires 4 additional arguments from the command line. Extra command is the
-number of nearest genes that you want.
-
-This program does not yet work perfectly. It's close but doesn't print the nearest genes
-in order. Further debugging required.
+This program requires 4 additional arguments from the command line: gtf file, chromosome of interest,
+position of interest, and the number of nearest genes that you want.
 """
 
 
@@ -26,28 +23,12 @@ def main():
     mut_chrom = sys.argv[2]
     mut_pos = int(sys.argv[3])
     num_genes = int(sys.argv[4])
-    f = open(gtf_file)
 
-    genes = []
-    for line in f:
-        if line.startswith("#"):
-            # Ignores header lines
-            continue
-        # Split line into a list and then define start and end
-        fields = line.strip("\r\n").split("\t")
-        start = int(fields[3])
-        end = int(fields[4])
-        
-        # Use line only if it is gene on our chromosome of interest
-        if (fields[0] == mut_chrom) and (fields[2] == "gene") and ('gene_biotype "protein_coding"' in line):
-            subfields = fields[-1].split(';') # Take last field
-            for field in subfields:
-                if "gene_name" in field:
-                    # Find 'gene name' and then take the string after
-                    gene_name = field.split()[1]
-            genes.append((gene_name, start, end)) # Builds list of tuples
+    genes = search.gtf_parser(gtf_file, mut_chrom)
     
     for i in range(num_genes):
+        # Performs same thing as in main() of search.py except does it multiple times
+        # once for each gene we want to find
         closest = search.binary_search(genes, mut_pos)
         idx = genes.index(closest[:3])
         genes.pop(idx)
--- day3-lunch/search.py
+++ day3-lunch/search.py
@@ -3,7 +3,8 @@
 Brendon Davis
 CMDB Bootcamp Day 3 Lunch
 
-This program requires 3 additional arguments from the command line.
+This program requires 3 additional arguments from the command line: gtf file, chromosome of interest,
+position of interest, and the number of nearest genes that you want.
 """
 
 
@@ -53,50 +54,38 @@ def binary_search(genes, pos, iteration=0):
     
     # Woohoo recursion is fun
     if dist == 0:
-        return (gene_name, gene_start, gene_end, dist, iteration)
+            return (gene_name, gene_start, gene_end, dist, iteration)
         
     elif pos < gene_start:
         if check_idx == 0:
             return (gene_name, gene_start, gene_end, dist, iteration)
-            
+        
         elif dist < distance_to_gene(genes[check_idx - 1], pos):
-            if encloses(genes[check_idx], genes[check_idx - 1]):
-                # Handles genes that encode other genes. Need for advanced exercises
-                # part 2 to work right
-                genes.pop(check_idx - 1)
-                return binary_search(genes, pos, iteration)
-            else:
-                return (gene_name, gene_start, gene_end, dist, iteration)
-                
+            return (gene_name, gene_start, gene_end, dist, iteration)
+            
         else:
             return binary_search(genes[:check_idx], pos, iteration)
-            
+    
+    # Same as previous elif except for reverse case
     elif pos > gene_end:
         if check_idx == (len(genes) - 1):
             return (gene_name, gene_start, gene_end, dist, iteration)
-            
+        
         elif dist < distance_to_gene(genes[check_idx + 1], pos):
-            if encloses(genes[check_idx], genes[check_idx + 1]):
-                genes.pop(check_idx + 1)
-                return binary_search(genes, pos, iteration)
-            else:
-                return (gene_name, gene_start, gene_end, dist, iteration)
-                
+            return (gene_name, gene_start, gene_end, dist, iteration)
+            
         else:
             return binary_search(genes[check_idx:], pos, iteration)
     
 
-def main():
+def gtf_parser(gtf_filename, mut_chrom):
     """
-    Parses .gtf file into ordered list of gene names and positions and then uses
-    that to find which gene is closest to a postion of interest on a particular
-    chromosome.
+    Takes name of gtf file and chromosome of interest, and parses gtf into a list of
+    tuples (gene_name, start, end) that includes only protein coding genes on the
+    chromosome of interest. Excludes duplicates or genes enclosed by other genes.
     """
-    # Read in arguments
-    gtf_file = sys.argv[1]
-    mut_chrom = sys.argv[2]
-    mut_pos = int(sys.argv[3])
-    f = open(gtf_file)
+    f = open(gtf_filename)
+    prev_gene = ('', 0, 0)
 
     genes = []
     for line in f:
@@ -109,14 +98,37 @@ def main():
         end = int(fields[4])
         
         # Use line only if it is gene on our chromosome of interest
+        # I modified this so it won't add 'duplicate' genes (genes that start and end at the same postion) or enclosed genes (genes enclosed by other genes)
         if (fields[0] == mut_chrom) and (fields[2] == "gene") and ('gene_biotype "protein_coding"' in line):
             subfields = fields[-1].split(';') # Take last field
             for field in subfields:
                 if "gene_name" in field:
                     # Find 'gene name' and then take the string after
                     gene_name = field.split()[1]
-            genes.append((gene_name, start, end)) # Builds list of tuples
+        
+                    current_gene = (gene_name, start, end)
+                    if not encloses(prev_gene, current_gene):
+                        # Checks to make sure this gene isn't enclosed by previous one
+                        genes.append(current_gene) # Builds list of tuples
+                        prev_gene = current_gene
     
+    return genes
+    
+    
+
+def main():
+    """
+    Parses .gtf file into ordered list of gene names and positions and then uses
+    that to find which gene is closest to a postion of interest on a particular
+    chromosome.
+    """
+    # Read in arguments
+    gtf_file = sys.argv[1]
+    mut_chrom = sys.argv[2]
+    mut_pos = int(sys.argv[3])
+
+    genes = gtf_parser(gtf_file, mut_chrom)
+
     closest = binary_search(genes, mut_pos) # No iteration param so that default is used
     print("The nearest gene is {}, which runs from positions {} to {}. "
            "It is {} base pairs from your position. This took {} "
